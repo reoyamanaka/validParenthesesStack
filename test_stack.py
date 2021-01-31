775f7da03ad20cc0870fa658e74303d6a296c92e
Reo Yamanaka <ryamanaka807@gmail.com>
2021-01-30 19:55:34 -0500

    created and finished unittest

---
+++ test_stack.py
@@ -0,0 +1,13 @@
+import unittest
+import validParenthesesStack as f
+
+class TestValidParenthesesStack(unittest.TestCase):
+    def test_validParentheses(self):
+        self.assertTrue(f.validP("({a+b})"), "Test value is True.")
+        self.assertFalse(f.validP("))((a+b}{"), "Test value is False.")
+        self.assertTrue(f.validP("((a+b))"), "Test value is True.")
+        self.assertFalse(f.validP("))"), "Test value is False.")
+        self.assertTrue(f.validP("[a+b]*(x+2y)*{gg+kk}"), "Test value is True.")
+        
+if __name__ == "__main__":
+    unittest.main()
